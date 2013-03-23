d9145a3c8708ee77d912a6eda6137b0763cc4b92
designinglloo <designinglloo@gmail.com>
2013-03-23 20:03:36 +0800

    Update index2.php

--- index2.php
+++ index2.php
@@ -1,7 +1,8 @@
 <?php
-    session_id(SID);           //session begins
-    session_start();
+  session_id(SID);           //session begins
+  session_start();
 ?>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN""http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
   <title>
@@ -10,7 +11,7 @@
   <script language=JavaScript><!--
     <!--FUNCTION TO OPEN SERIES.HTML FROM 'SERIES' added by jiao-->
     function openseries(series_name){
-      series_path="http://japaneseproducthere.sitemix.jp/series/"+series_name+"/";
+      series_path="/series/"+series_name+"/";
       strhtml="";
       for(i=1;i<=8;i=i+1){
         strhtml=strhtml + "<img onclick='openproduct("+"\""+series_name+"_"+i+"\""+");' width=20% height=8% style='margin-bottom:5px;' scrolling='no' src="+series_path+series_name+"_"+i+"/"+series_name+"_"+i+".jpg></img><iframe height=8% style='margin-left:5px;border-width:0;' src="+series_path+series_name+"_"+i+"/"+series_name+"_"+i+"_title_sc.txt></iframe>" + "<br>";
@@ -20,7 +21,7 @@
     function openproduct(product_num){
       //take care of the "" and ''(use '' outside and "" inside); check spaces if buttons shows incorrectly;
       //here to edit html of PRODUCT information 
-      document.getElementById("change_item").innerHTML='<form name="product_form"><img width="40%" src="http://japaneseproducthere.sitemix.jp/series/shangpintu/' + product_num + '/' + product_num + '.jpg"></img><iframe frameborder=0 scrolling=0 width="50%" src="http://japaneseproducthere.sitemix.jp/series/shangpintu/' + product_num + '/' + product_num + '_information_sc.txt"></iframe><br><br><div style="background-color:#efab93;width=100%;padding-top:10px;margin-bottom:0;margin-top:10px;margin-left:0;color:#fef4f4;"><input type="checkbox">&#25918;&#20837;&#36141;&#29289;&#36710;  </input><input type="button" id="jiekuan" onmouseover=mouseoveroflogin("jiekuan"); onmouseout=mouseoutoflogin("jiekuan"); class="login_button_style" value=&#32467;&#31639;> </input><input type="button" id="jixugouwu" onmouseover=mouseoveroflogin("jixugouwu"); onmouseout=mouseoutoflogin("jixugouwu"); class="login_button_style" value=&#32487;&#32493;&#36141;&#29289;></input></div><br><br><iframe frameborder=0 scrolling=0 width="100%" src="http://japaneseproducthere.sitemix.jp/series/shangpintu/' + product_num + '/' + product_num + '_sc.txt"></iframe></form>';
+      document.getElementById("change_item").innerHTML='<form name="product_form"><img width="40%" src="/series/shangpintu/' + product_num + '/' + product_num + '.jpg"></img><iframe frameborder=0 scrolling=0 width="50%" src="/series/shangpintu/' + product_num + '/' + product_num + '_information_sc.txt"></iframe><br><br><div style="background-color:#efab93;width=100%;padding-top:10px;margin-bottom:0;margin-top:10px;margin-left:0;color:#fef4f4;"><input type="checkbox">&#25918;&#20837;&#36141;&#29289;&#36710;  </input><input type="button" id="jiekuan" onmouseover=mouseoveroflogin("jiekuan"); onmouseout=mouseoutoflogin("jiekuan"); class="login_button_style" value=&#32467;&#31639;> </input><input type="button" id="jixugouwu" onmouseover=mouseoveroflogin("jixugouwu"); onmouseout=mouseoutoflogin("jixugouwu"); class="login_button_style" value=&#32487;&#32493;&#36141;&#29289;></input></div><br><br><iframe frameborder=0 scrolling=0 width="100%" src="/series/shangpintu/' + product_num + '/' + product_num + '_sc.txt"></iframe></form>';
     }
     function mouseover(element_name){
       document.getElementById(element_name).className="button_mouse_moving_on";
@@ -82,51 +83,54 @@
 	  }
 	}
 	function register_id(){
-	  document.getElementById("change_item").innerHTML='<div style="background-color:#f5b1aa;color:#fef4f4;padding:20px;"><form id="id_register" method=post action="http://japaneseproducthere.sitemix.jp/register.php" onsubmit="return check_register_infor();">&#26165;&#31216;:<input type=text id="user_id_text" style="background-color:#fef4f4;border-width=0;outline-style:none;"></input>(&#33521;&#23383;&#27597;/&#25968;&#23383;6&#23383;&#33410;&#20197;&#19978;&#65292;&#35831;&#20197;&#33521;&#23383;&#27597;&#24320;&#22836;)<br>&#37038;&#31665;:<input type=text id="user_email_text" style="background-color:#fef4f4;border-width=0;outline-style:none;"></input>(&#35831;&#36755;&#20837;&#27491;&#30830;&#30340;&#37038;&#31665;&#22320;&#22336;)<br>&#37038;&#31665;:<input type=text id="user_email_text_config" style="background-color:#fef4f4;border-width=0;outline-style:none;"></input>(&#20877;&#27425;&#36755;&#20837;&#65292;&#20197;&#20316;&#30830;&#35748;)<br><br>&#23494;&#30721;:<input type=password id="user_pass_text" style="background-color:#fef4f4;border-width=0;outline-style:none;"></input>(&#33521;&#23383;&#27597;/&#25968;&#23383;6&#23383;&#33410;&#20197;&#19978;)<br>&#23494;&#30721;:<input type=password id="user_pass_text_config" style="background-color:#fef4f4;border-width=0;outline-style:none;"></input>(&#20877;&#27425;&#36755;&#20837;&#65292;&#20197;&#20316;&#30830;&#35748;)<br><br><input type=submit id="register_submit" onmouseover=mouseoveroflogin("register_submit"); onmouseout=mouseoutoflogin("register_submit"); class="login_button_style" style="width:15%;" value=&#25552;&#20132;></input><div id="register_warning"></div></form></div>';
+	  document.getElementById("change_item").innerHTML='<div style="background-color:#f5b1aa;color:#fef4f4;padding:20px;"><form id="id_register" method=post action="register.php" onsubmit="return check_register_infor();">&#26165;&#31216;:<input type=text id="user_id_text" name="user_id_text" style="background-color:#fef4f4;border-width:0;margin-bottom:10px;"></input>(&#33521;&#23383;&#27597;/&#25968;&#23383;6&#23383;&#33410;&#20197;&#19978;&#65292;&#35831;&#20197;&#33521;&#23383;&#27597;&#24320;&#22836;)<br>&#37038;&#31665;:<input type=text id="user_email_text" name="user_email_text" style="background-color:#fef4f4;border-width:0;margin-bottom:5px;"></input>(&#35831;&#36755;&#20837;&#27491;&#30830;&#30340;&#37038;&#31665;&#22320;&#22336;)<br>&#37038;&#31665;:<input type=text id="user_email_text_config" style="background-color:#fef4f4;border-width:0;margin-bottom:5px;"></input>(&#20877;&#27425;&#36755;&#20837;&#65292;&#20197;&#20316;&#30830;&#35748;)<br><br>&#23494;&#30721;:<input type=password id="user_pass_text" name="user_pass_text" style="background-color:#fef4f4;border-width:0;margin-bottom:5px;"></input>(&#33521;&#23383;&#27597;/&#25968;&#23383;6&#23383;&#33410;&#20197;&#19978;)<br>&#23494;&#30721;:<input type=password id="user_pass_text_config" style="background-color:#fef4f4;border-width:0;margin-bottom:5px;"></input>(&#20877;&#27425;&#36755;&#20837;&#65292;&#20197;&#20316;&#30830;&#35748;)<br><br><input type=submit id="register_submit" onmouseover=mouseoveroflogin("register_submit"); onmouseout=mouseoutoflogin("register_submit"); class="login_button_style" style="width:15%;" value=&#25552;&#20132;></input><div id="register_warning"></div></form></div>';
 	}
   //--></script>
-  <link rel="stylesheet" type="text/css" href="http://japaneseproducthere.sitemix.jp/necessary/sitestyle.css" /> 
+  <link rel="stylesheet" type="text/css" href="/necessary/sitestyle.css" /> 
 </head>
 <body bgcolor=#fef4f4 onunload="session.clear();">
 
-<table width="70%" align="center" bgcolor="#fdeff2" border=0>
+<table width="75%" align="center" bgcolor="#fdeff2" 
   <tr align="right">
     <td width="25%" align="left">
-      <a href="http://japaneseproducthere.sitemix.jp/index2.php">&#36820;&#22238;&#20027;&#39029;</html>
+      <a href="index.html">&#36820;&#22238;&#20027;&#39029;</html>
     </td>
-    <td align="right">
+    <td align="right" width="75%">
       <!--LANGUAGE added by jiao-->
-      <a href="http://japaneseproducthere.sitemix.jp/en/index-e.html">English</a>
-      <a href="http://japaneseproducthere.sitemix.jp/sc/index-sc.html">  &#20013;&#25991;&#31616;&#20307;</a>
-      <a href="http://japaneseproducthere.sitemix.jp/tc/index-tc.html">  &#32321;&#39636;&#20013;&#25991;</a>
-      <a href="http://japaneseproducthere.sitemix.jp/jp/index-j.html">  &#26085;&#26412;&#35486;</a>
+      <a href="index5.php">English</a>
+      <a href="index2.php">  &#20013;&#25991;&#31616;&#20307;</a>
+      <a href="index3.php">  &#32321;&#39636;&#20013;&#25991;</a>
+      <a href="index4.php">  &#26085;&#26412;&#35486;</a>
     </td>
   </tr>
   <tr>
-    <td></td>
+    <td width="25%"></td>
     <td align="middle">
       <!--TITLE OF THE SITE(SHOULD BE A PICTURE) added by jiao-->
       <font size=7 >AkumaRose</font>
     </td>
   </tr>
   <tr>
-    <td valign="top">
+    <td valign="top" width="25%">
 	  <!--LOGGINFORM added by jiao-->
       <?php
         if(isset($_SESSION['user_id'])){  //when password is correct
           echo '
-          <form name="log_out" action="http://japaneseproducthere.sitemix.jp/logout.php" method="post" style="background-color:#efab93;margin-bottom:0;">
+          <form name="log_out" action="logout.php" method="post" style="background-color:#efab93;margin-bottom:0;">
             <font style="color:#fef4f4;margin-left:10px;">Welcome!&nbsp;&nbsp;'.$_SESSION['user_id'].'</font><br>
             <input type="submit" id="logout" onmouseover=mouseoveroflogin("logout"); onmouseout=mouseoutoflogin("logout"); class="login_button_style" style="margin-top:5px;margin-left:10px;" value=&#30331;&#20986;>
           </form>'; //logout button and (mypage button)
         }
         else{
-          if (isset($_SESSION['pass_incorrect'])){echo "password incorrect";}  //when password is not correct
+          if (isset($_SESSION['pass_incorrect'])){
+		    echo "id or password incorrect";
+			unset($_SESSION['pass_incorrect']);
+		  }  //when password is not correct
           echo "
-            <form name='log_in' action='http://japaneseproducthere.sitemix.jp/login.php' method='post' style='background-color:#efab93;color:#fef4f4;margin-bottom:0;padding-left:10px;'>
+            <form name='log_in' action='login.php' method='post' style='background-color:#efab93;color:#fef4f4;margin-bottom:0;padding-left:10px;'>
               <br>
-              &#29992;&#25143;&#21517;: <input type='text' id='login_id' style='background-color:#fef4f4;border-width:0;width:50%;'>
-              &#23494;&nbsp;&nbsp;&#30721;: <input type='text' id='login_pass' style='background-color:#fef4f4;border-width:0;width:50%;margin-bottom:10px;'> <br>
+              &#26165;&nbsp;&nbsp;&#31216;: <input type='text' id='login_id'  name='login_id' style='background-color:#fef4f4;border-width:0;width:50%;margin-bottom:5px;'>
+              &#23494;&nbsp;&nbsp;&#30721;: <input type='password' id='login_pass'  name='login_pass' style='background-color:#fef4f4;border-width:0;width:50%;margin-bottom:10px;'> <br>
               <input type='submit' id='login' onmouseover=mouseoveroflogin('login'); onmouseout=mouseoutoflogin('login'); class='login_button_style' value=&#30331;&#24405;>
               <input type='button' id='signin' onclick='register_id();' onmouseover=mouseoveroflogin('signin'); onmouseout=mouseoutoflogin('signin') class='login_button_style' value=&#27880;&#20876;>
               <br>
@@ -169,31 +173,35 @@
       </form>
       <!--INFORMATION FORM END added by jiao-->
     </td>
-    <td id="change_item" valign="top">
+    <td id="change_item" valign="top" width="75%">
 	  <?php
 	    if(isset($_SESSION['submit_succeed'])){
 		  if($_SESSION['submit_succeed']==1){
 		    echo '&#27880;&#20876;&#25104;&#21151;!';
 		  }
-		  else{
+		  else if ($_SESSION['submit_succeed']==0){
 		    echo '&#27880;&#20876;&#19981;&#25104;&#21151;&#65292;&#35831;&#31245;&#21518;&#20877;&#27425;&#27880;&#20876;&#12290;&#12290;&#12290;';
+		  }
+		  else if($_SESSION['submit_succeed']==2){
+		    echo '&#29992;&#25143;&#21517;&#24050;&#34987;&#20182;&#20154;&#27880;&#20876; :(<br>';
+			echo "<input type='button' id='signin_again' onclick='register_id();' onmouseover=mouseoveroflogin('signin_again'); onmouseout=mouseoutoflogin('signin_again') class='login_button_style' value=&#20877;&#27425;&#27880;&#20876;>";
 		  }
 		  unset($_SESSION['submit_succeed']);
 		}
 		else{//take care of the dealing with "" and ''
 		  echo'
             <!--PICTURES (FROM "PICTURES")added by jiao-->  
-            <img onclick="openproduct('.'\''.'baoshiruangao_1'.'\''.');" src="http://japaneseproducthere.sitemix.jp/pictures/1.jpg" width="90%"></img><br>
-            <img src="http://japaneseproducthere.sitemix.jp/pictures/2.jpg" width=30%></img>
-            <img src="http://japaneseproducthere.sitemix.jp/pictures/3.jpg" width=30%></img><br><br>
-            <img src="http://japaneseproducthere.sitemix.jp/pictures/4.jpg" width=30%></img>
-            <img src="http://japaneseproducthere.sitemix.jp/pictures/5.jpg" width=30%></img>
-            <img src="http://japaneseproducthere.sitemix.jp/pictures/6.jpg" width=30%></img><br><br>
-            <img src="http://japaneseproducthere.sitemix.jp/pictures/7.jpg" width=30%></img>
-            <img src="http://japaneseproducthere.sitemix.jp/pictures/8.jpg" width=30%></img><br><br>
-            <img src="http://japaneseproducthere.sitemix.jp/pictures/9.jpg" width=30%></img>
-            <img src="http://japaneseproducthere.sitemix.jp/pictures/10.jpg" width=30%></img>
-            <img src="http://japaneseproducthere.sitemix.jp/pictures/11.jpg" width=30%></img><br><br>
+            <img onclick="openproduct('.'\''.'baoshiruangao_1'.'\''.');" src="/pictures/1.jpg" width="90%"></img><br>
+            <img src="/pictures/2.jpg" width=30%></img>
+            <img src="/pictures/3.jpg" width=30%></img><br><br>
+            <img src="/pictures/4.jpg" width=30%></img>
+            <img src="/pictures/5.jpg" width=30%></img>
+            <img src="/pictures/6.jpg" width=30%></img><br><br>
+            <img src="/pictures/7.jpg" width=30%></img>
+            <img src="/pictures/8.jpg" width=30%></img><br><br>
+            <img src="/pictures/9.jpg" width=30%></img>
+            <img src="/pictures/10.jpg" width=30%></img>
+            <img src="/pictures/11.jpg" width=30%></img><br><br>
             <!--PICTURES END added by jiao-->';
 		}
 	  ?>
