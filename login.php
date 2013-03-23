008a8d7e20fdf36ad44d400028a48e891c26a685
designinglloo <designinglloo@gmail.com>
2013-03-23 20:05:27 +0800

    Update login.php

--- login.php
+++ login.php
@@ -2,18 +2,26 @@
   <body>
     <?php
       session_start();
-      //unset($_SESSION['user_id']);
-      //unset($_SESSION['pass_incorrect']);
-      //connect the database
-      $pass_correct=1;         //$pass_correct to record whether the password & 
-
-id are correct
-      if($pass_correct==1){
-        $_SESSION['user_id']='oneguest';
+      unset($_SESSION['user_id']);
+      unset($_SESSION['pass_incorrect']);
+    $con=mysql_connect("localhost","***","***");
+	  if (!$con){
+          die('Could not connect: ' . mysql_error());
+        }
+	  mysql_select_db("akumarose_db",$con);
+	  $result=mysql_query("SELECT * FROM id_pass where user_id='$_POST[login_id]'",$con);
+	  $row=mysql_fetch_array($result);
+	  $real_pass= $row['user_pass'];
+	
+      if($real_pass==$_POST['login_pass'] && $real_pass !=""){
+        $_SESSION['user_id']=$_POST['login_id'];
       }else{
         $_SESSION['pass_incorrect']=1;
       }
-      header('location:http://japaneseproducthere.sitemix.jp/index2.php');
+      header('location:index2.php');
     ?>
   </body>
 </html>
+
+
+
